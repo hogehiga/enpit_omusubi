6aa1cf648db8f340b7c8c9b5fd281dd4cab46e0b
松岡隆斗 <>
2020-10-13 18:15:46 +0900

    imageテーブル作成。写真の表示方法の変更。
    写真と、地名を保存するためのテーブルを作成。保存したテーブルの情報を使って画像を表示できるようにした。

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_13_082508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.text "image_path"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
