b3db6f071ed88041bda882f41f952d0c6e55b4a4
hogehiga <hogehiga@gmail.com>
2020-10-20 09:26:54 +0900

    最短経路探索_2つ

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -9,4 +9,9 @@ class ImageTest < ActiveSupport::TestCase
     route = Image.approximate_shortest_route([images(:syurijou)].map(&:id))
     assert_equal route.length, 1
   end
+
+  test "approximate_shortest_route_2枚" do
+    route = Image.approximate_shortest_route([images(:syurijou), images(:kourijima)].map(&:id))
+    assert_equal route.length, 2 # 2なので順序は問わない
+  end
 end
