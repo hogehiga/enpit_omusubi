115c8acdf4af5b9b365cebf4b78ee24dddeeddac
hogehiga <hogehiga@gmail.com>
2020-10-27 10:49:49 +0900

    データ構造が変わったのでテスト修正。

--- test/controllers/tops_controller_test.rb
+++ test/controllers/tops_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TopsControllerTest < ActionDispatch::IntegrationTest
   test "画像選択なし" do
-    post root_url
+    post root_url, params: { navigate_form: { images: { images(:syurijou).id.to_s => "0" } } }
     assert_response :success
     assert response.body.include?("画像を選択してください")
   end
 
   test "画像選択1枚_首里城" do
-    post root_url, params: { images: [images(:syurijou).id]}
+    post root_url, params: { navigate_form: { images: { images(:syurijou).id.to_s => "1" } } }
     assert_response :redirect
   end
 end
