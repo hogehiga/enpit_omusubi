33c6db1892639794d501ede56aaf883b76e790d7
hogehiga <hogehiga@gmail.com>
2020-10-30 16:01:43 +0900

    動作確認用データ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Image.create!([
+  {name: "古宇利島", image_path: "kirei.jpg", lat: 26.7042905, lng: 128.0094242},
+  {name: "首里城", image_path: "kirei_shurijo.jpg", lat: 26.2183181, lng: 127.7131627},
+  {name: "奥武島", image_path: "Ou_Island.jpg", lat: 26.1293207, lng: 127.7637},
+  {name: "那覇バスターミナル", image_path: "naha_bus_terminal.jpg", lat: 26.2110156, lng: 127.6742415},
+  {name: "那覇空港", image_path: nil, lat: 26.2064033, lng: 127.6443535}
+])
