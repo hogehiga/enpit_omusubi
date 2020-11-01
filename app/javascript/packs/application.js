96133abffbca67efdbab8610af0c079e9e2d0e7e
hogehiga <hogehiga@gmail.com>
2020-11-01 10:02:10 +0900

    https://github.com/rails/webpacker#usage によると、app/javascript/packsにはentry fileだけ置くようにとのことです。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery') //追記
-require('packs/top.js')
+require('top')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
